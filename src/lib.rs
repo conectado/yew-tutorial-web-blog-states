7785f7f67b1571575ec6a41ad0ad05a76948246c
conectado <gabrielalejandro7@gmail.com>
2020-08-25 20:05:44 -0300

    Add request loader

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
-use markdown_visualizer::MarkdownVisualizer;
+use request_loader::{RequestLoader, RequestLoaderProps};
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
 mod markdown_visualizer;
+mod request_loader;
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    App::<MarkdownVisualizer>::new().mount_to_body();
+    App::<RequestLoader>::new().mount_as_body_with_props(RequestLoaderProps {
+        url: "/articles/test.md".to_string(),
+    });
 }
--- src/markdown_visualizer.rs
+++ src/markdown_visualizer.rs
@@ -2,31 +2,6 @@ use pulldown_cmark as pc;
 use yew::prelude::*;
 use yew::web_sys;
 
-pub struct MarkdownVisualizer;
-
-impl Component for MarkdownVisualizer {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_props: Self::Properties, _link: ComponentLink<Self>) -> Self {
-        MarkdownVisualizer
-    }
-
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
-        false
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            {view_markdown("# This is bold!")}
-        }
-    }
-}
-
 fn create_markdown_container() -> web_sys::Element {
     let window = web_sys::window().expect("Can't find window");
     let document = window.document().expect("Can't find document");
@@ -35,7 +10,7 @@ fn create_markdown_container() -> web_sys::Element {
     div
 }
 
-fn view_markdown(value: &str) -> Html {
+pub fn view_markdown(value: &str) -> Html {
     let parser = pc::Parser::new(value);
     let mut html_output = String::new();
     pc::html::push_html(&mut html_output, parser);
