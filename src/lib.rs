ae425650bc4040ae1df186b4bf23973683be47d2
conectado <gabrielalejandro7@gmail.com>
2020-08-25 03:32:39 -0300

    Add hello world component

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,32 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct HelloWorld;
+
+impl Component for HelloWorld {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        HelloWorld
     }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <p>{"Hello, world!"}</p>
+        }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<HelloWorld>::new().mount_to_body();
 }
