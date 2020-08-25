bf1310514993cf198c09f15d991fc1121ab58ffe
conectado <gabrielalejandro7@gmail.com>
2020-08-25 03:38:32 -0300

    Add markdown visualizer

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,10 @@
+use markdown_visualizer::MarkdownVisualizer;
 use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
-struct HelloWorld;
-
-impl Component for HelloWorld {
-    type Message = ();
-    type Properties = ();
-
-    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
-        HelloWorld
-    }
-
-    fn update(&mut self, _: Self::Message) -> ShouldRender {
-        false
-    }
-
-    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        false
-    }
-
-    fn view(&self) -> Html {
-        html! {
-            <p>{"Hello, world!"}</p>
-        }
-    }
-}
+mod markdown_visualizer;
 
 #[wasm_bindgen(start)]
 pub fn run_app() {
-    App::<HelloWorld>::new().mount_to_body();
+    App::<MarkdownVisualizer>::new().mount_to_body();
 }
